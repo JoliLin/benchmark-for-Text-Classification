276533762a2a2c06434cffd51bf112c3a566952f
JoliLin <JoliLin@users.noreply.github.com>
2019-08-08 17:31:28 +0800

    Update RUN_rt.py

--- code/RUN_rt.py
+++ code/RUN_rt.py
@@ -2,7 +2,7 @@ import torch_main
 import rt_data as rt
 
 #rt-polarity
-path = '/home/zllin/torch_exp_flow/dataset/rt-polarity.all'
+path = 'dataset/rt-polarity.all'
 data = rt.rt_data(path).load_data()
 classes = 2
 	
